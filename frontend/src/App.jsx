4d0a50ee13021874db7d8768e3c91c10eb2b3656
shyam kumar <sk5859915@gmail.com>
2025-04-26 12:49:58 +0530

    adding  signup login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'  
 import Home from './pages/Home'
 import UserLogin from './pages/UserLogin'
+import CaptainSignup from './pages/CaptainSignup'
+import UserSignup from './pages/UserSignup'
+import Captainlogin from './pages/Captainlogin'
 
 const App = () => {
   return (
@@ -9,6 +12,11 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />         
         <Route path="/login" element={<UserLogin />} /> 
+
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/" element={<UserLogin />} />
+        <Route path='/captain-signup' element={<CaptainSignup />} />
+        <Route path='/captain-login' element={<Captainlogin />} />
       </Routes>
     </div>
   )
